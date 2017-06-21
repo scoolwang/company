5ecc3ef2e4a10e3e131498df20dda04cb28befa2
王甜爽 <467637042@qq.com>
2017-06-21 19:01:47 +0800

    项目提交

--- webpack.config.js
+++ webpack.config.js
@@ -13,8 +13,43 @@ module.exports = {
     plugins: [
       new htmlWebpackPlugin({
         title: 'My App',
+        filename: 'artical.html',
+        template: './html/artical.html'
+      }),
+      new htmlWebpackPlugin({
+        title: 'My App',
+        filename: 'news.html',
+        template: './html/news.html'
+      }),     
+      new htmlWebpackPlugin({
+        title: 'My App',
+        filename: 'call.html',
+        template: './html/call.html'
+      }),   
+      new htmlWebpackPlugin({
+        title: 'My App',
         filename: 'company.html',
         template: './html/company.html'
+      }),
+      new htmlWebpackPlugin({
+        title: 'My App',
+        filename: 'business.html',
+        template: './html/business.html'
+      }),
+      new htmlWebpackPlugin({
+        title: 'My App',
+        filename: 'index.html',
+        template: './html/index.html'
+      }),
+      new htmlWebpackPlugin({
+        title: 'My App',
+        filename: 'index.html',
+        template: './html/call.html'
+      }),
+      new htmlWebpackPlugin({
+        title: 'My App',
+        filename: 'join.html',
+        template: './html/join.html'
       })
     ]
 };
